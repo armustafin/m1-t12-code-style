062c2bf61af0d24691b180524370d9a642d250be
iceekb <123372323+AndreyAFedotov@users.noreply.github.com>
2023-02-20 20:38:39 +0300

    Update DepositCalculator.java
    Совместное ревью

--- src/DepositCalculator.java
+++ src/DepositCalculator.java
@@ -1,11 +1,11 @@
 import java.util.Scanner;
 
 public class DepositCalculator {
-    double calculateComplexPercent(double amount, double percent, int depositPeriod) {
-       double pay = amount * Math.pow((1 + percent / 12), 12 * depositPeriod);
+    double calculateComplexPercent(double amount, double percent, int depositPeriod) { // Думаю стоит все три переменные сигнатуры к одному виду привести
+       double pay = amount * Math.pow((1 + percent / 12), 12 * depositPeriod);         // либо всем deposit вначало, либо нет. Может ввести в заблуждение
        return roundedOfAmount(pay, 2);
     }
-    double calculateSimplePercent(double Amount, double yearRate, int depositPeriod) {
+    double calculateSimplePercent(double Amount, double yearRate, int depositPeriod) { // То же самое, что и с предыдущим методом
          return roundedOfAmount(Amount + Amount * yearRate * depositPeriod, 2);
     }
     double roundedOfAmount(double value, int places) {
@@ -13,7 +13,7 @@ public class DepositCalculator {
        return Math.round(value * scale) / scale;
     }
 
-    void calculateAmount() {
+    void calculateAmount() { // В самом методе непосредственно расчета не происходит. Он по сути ни что иное как интерфейс взаимодейтсвия.
         int depositPeriod;
         int chooseAction;
         Scanner scanner = new Scanner(System.in);
@@ -31,6 +31,8 @@ public class DepositCalculator {
             calculatedAmountPercent = calculateComplexPercent(amount, 0.06, depositPeriod);
         }
         System.out.println("Результат вклада: " + amount + " за " + depositPeriod + " лет превратятся в " + calculatedAmountPercent);
+        // Длинна строки превышает 120 знаков, необходим перенос на новую строку с учетом правил переноса
+        // Вопрос с именованием ключевых переменных тот же, что и в иетодах расчета
     }
     public static void main(String[] args){
         new DepositCalculator().calculateAmount();
